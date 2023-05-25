4c024b04309faddf10c29229cb55b66c4b4e4826
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-05-25 16:38:58 +0530

    Fix. Map Loading logic

--- ThanosTrackr/assets/js/script.js
+++ ThanosTrackr/assets/js/script.js
@@ -1,105 +1,196 @@
-var map = new maplibregl.Map({
-    container: 'map',
-    style:
-        'https://api.maptiler.com/maps/streets/style.json?key=ay1b2HnVwvraYQcRpld9',
-    center: [78.491684, 17.387140],
-    zoom: 7.5
-});
-
-var size = 200;
-
-// implementation of StyleImageInterface to draw a pulsing dot icon on the map
-// see https://maplibre.org/maplibre-gl-js-docs/api/properties/#styleimageinterface for more info
-var pulsingDot = {
-    width: size,
-    height: size,
-    data: new Uint8Array(size * size * 4),
-
-    // get rendering context for the map canvas when layer is added to the map
-    onAdd: function () {
-        var canvas = document.createElement('canvas');
-        canvas.width = this.width;
-        canvas.height = this.height;
-        this.context = canvas.getContext('2d');
+var map;
+var pulsingDot;
+var stones = [
+    {
+        name: 'Mind Stone',
+        image: 'assets/images/Mind_Stone_VFX.webp',
+        location: ([72.8777, 19.0760])
     },
-
-    // called once before every frame where the icon will be used
-    render: function () {
-        var duration = 1000;
-        var t = (performance.now() % duration) / duration;
-
-        var radius = (size / 2) * 0.3;
-        var outerRadius = (size / 2) * 0.7 * t + radius;
-        var context = this.context;
-
-        // draw outer circle
-        context.clearRect(0, 0, this.width, this.height);
-        context.beginPath();
-        context.arc(
-            this.width / 2,
-            this.height / 2,
-            outerRadius,
-            0,
-            Math.PI * 2
-        );
-        context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
-        context.fill();
-
-        // draw inner circle
-        context.beginPath();
-        context.arc(
-            this.width / 2,
-            this.height / 2,
-            radius,
-            0,
-            Math.PI * 2
-        );
-        context.fillStyle = 'rgba(255, 100, 100, 1)';
-        context.strokeStyle = 'white';
-        context.lineWidth = 2 + 4 * (1 - t);
-        context.fill();
-        context.stroke();
-
-        // update this image's data with data from the canvas
-        this.data = context.getImageData(
-            0,
-            0,
-            this.width,
-            this.height
-        ).data;
-
-        // continuously repaint the map, resulting in the smooth animation of the dot
-        map.triggerRepaint();
-
-        // return `true` to let the map know that the image was updated
-        return true;
+    {
+        name: 'Power Stone',
+        image: 'assets/images/Power_Stone_VFX.webp',
+        location: [67.8777, 29.0760]
+    },
+    {
+        name: 'Reality Stone',
+        image: 'assets/images/Reality_Stone_VFX.webp',
+        location: [72.8777, 19.0760]
+    },
+    {
+        name: 'Soul Stone',
+        image: 'assets/images/Soul_Stone_VFX.webp',
+        location: [72.8777, 19.0760]
+    },
+    {
+        name: 'Space Stone',
+        image: 'assets/images/Space_Stone_VFX.webp',
+        location: [72.8777, 19.0760]
+    },
+    {
+        name: 'Time Stone',
+        image: 'assets/images/Time_Stone_VFX.webp',
+        location: [72.8777, 19.0760]
     }
-};
+];
+var popup;
 
-map.on('load', function () {
-    map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+function initializeMap(centerCoordinates) {
+    map = new maplibregl.Map({
+        container: 'map',
+        style: 'https://api.maptiler.com/maps/streets/style.json?key=ay1b2HnVwvraYQcRpld9',
+        center: centerCoordinates,
+        zoom: 1,
+    });
+}
 
-    map.addSource('points', {
-        'type': 'geojson',
-        'data': {
-            'type': 'FeatureCollection',
-            'features': [
+function initializePulsingDot() {
+    var size = 200;
+
+    pulsingDot = {
+        width: size,
+        height: size,
+        data: new Uint8Array(size * size * 4),
+
+        onAdd: function () {
+            var canvas = document.createElement('canvas');
+            canvas.width = this.width;
+            canvas.height = this.height;
+            this.context = canvas.getContext('2d');
+        },
+
+        render: function () {
+            var duration = 1000;
+            var t = (performance.now() % duration) / duration;
+
+            var radius = (size / 2) * 0.3;
+            var outerRadius = (size / 2) * 0.7 * t + radius;
+            var context = this.context;
+
+            context.clearRect(0, 0, this.width, this.height);
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                outerRadius,
+                0,
+                Math.PI * 2
+            );
+            context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+            context.fill();
+
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                radius,
+                0,
+                Math.PI * 2
+            );
+            context.fillStyle = 'rgba(255, 100, 100, 1)';
+            context.strokeStyle = 'white';
+            context.lineWidth = 2 + 4 * (1 - t);
+            context.fill();
+            context.stroke();
+
+            this.data = context.getImageData(
+                0,
+                0,
+                this.width,
+                this.height
+            ).data;
+
+            map.triggerRepaint();
+
+            return true;
+        }
+    };
+}
+
+function initializePopup() {
+    popup = new maplibregl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
+}
+
+function showMapAndStones(stoneIndex) {
+    console.log("Stone Index: " + stoneIndex);
+    let coordinates = stones[stoneIndex].location;
+    let stoneName = stones[stoneIndex].name;
+
+    if (!map) {
+        initializeMap([0, 0]);
+        initializePulsingDot();
+        initializePopup();
+
+        map.on('load', function () {
+            map.addImage('pulsing-dot-' + stoneName, pulsingDot, { pixelRatio: 2 });
+        });
+    }
+
+    if (map.getSource('points-' + stoneName)) {
+        map.removeLayer('points-' + stoneName);
+        map.removeSource('points-' + stoneName);
+    }
+
+    map.addImage('pulsing-dot-' + stoneName, pulsingDot, { pixelRatio: 2 });
+    map.addSource('points-' + stoneName, {
+        type: 'geojson',
+        data: {
+            type: 'FeatureCollection',
+            features: [
                 {
-                    'type': 'Feature',
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': [78.491684, 17.387140]
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: coordinates
                     }
                 }
             ]
         }
     });
+
     map.addLayer({
-        'id': 'points',
-        'type': 'symbol',
-        'source': 'points',
-        'layout': {
-            'icon-image': 'pulsing-dot'
+        id: 'points-' + stoneName,
+        type: 'symbol',
+        source: 'points-' + stoneName,
+        layout: {
+            'icon-image': 'pulsing-dot-' + stoneName
         }
     });
-});
+
+    map.flyTo({ center: coordinates, zoom: 7 });
+
+    map.on('mouseenter', 'points-' + stoneName, function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', 'points-' + stoneName, function () {
+        map.getCanvas().style.cursor = '';
+    });
+
+    map.on('mouseover', 'points-' + stoneName, function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var stone = stones[stoneIndex]; // Replace with your logic to determine the stone based on coordinates
+
+        popup.setLngLat(coordinates)
+            .setHTML(
+                '<div style="text-align:center" id="hover-window">' +
+                '<img src="' + stone.image + '" alt="' + stone.name + '" style="max-width: 100px; max-height: 100px;"/>' +
+                '<h5>' + stone.name + '</h5>' +
+                '</div>'
+            )
+            .addTo(map);
+    });
+
+    map.on('mouseout', 'points-' + stoneName, function () {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
+}
+
+
+// Initialize the map when the page loads
+initializeMap([0, 0]);
+initializePulsingDot();
+initializePopup();
